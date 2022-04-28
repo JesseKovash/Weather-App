313a7f01aa5f1988471d2ef4dc0e4cba14be5463
Jesse Kovash <jessekovash@gmail.com>
2022-04-28 16:15:12 -0600

    made changes to 7 day. top banner correctly rendering. next step is to display weather for the next 7 days

--- myapp/src/components/sevenDay.jsx
+++ myapp/src/components/sevenDay.jsx
@@ -1,31 +1,32 @@
 import React, { useContext } from "react";
 import { hot } from 'react-hot-loader/root';
-import {LocationContext} from '../App.js'
+import { LocationContext } from '../App.js'
 
-function SevenDayForecast (props) {
+function SevenDayForecast(props) {
   const Values = useContext(LocationContext);
   let today;
   let future;
   if (Values.currentLocation !== null) {
     console.log(Values.weatherInfo)
     today =
-      <div>
-        <h2>7 Day Weather - {Values.currentLocation.city}</h2>
+      <div className="seven_top_container">
+        <h2>7 Day Weather
+          <span>-{Values.currentLocation.city}, {Values.currentLocation.state}</span>
+        </h2>
         <h3>As of {Values.weatherInfo.retrievedTime}</h3>
         <div className="seven_today_details">
           <p>Need to add Today's date</p>
           <div className="seven_temp_icon">
-          <p className="seven_today_temp">{
-            Values.tempScale === 'F' ?
-              (Values.toFahrenheit(Values.weatherInfo.current.temp)) + ' F' :
-              (Values.toCelsius(Values.weatherInfo.current.temp)) + 'C'}
-          </p>
-          <img src={`http://openweathermap.org/img/wn/${Values.weatherInfo.current.weather[0].icon}@2x.png`}></img>
+            <p className="seven_today_temp">{
+              Values.tempScale === 'F' ?
+                (Values.toFahrenheit(Values.weatherInfo.current.temp)) + ' F' :
+                (Values.toCelsius(Values.weatherInfo.current.temp)) + 'C'}
+            </p>
+            <img src={`http://openweathermap.org/img/wn/${Values.weatherInfo.current.weather[0].icon}@2x.png`}></img>
           </div>
-          <p>{Values.weatherInfo.current.weather[0].description}</p>
-
+          <p className="seven_today_desc">{Values.weatherInfo.current.weather[0].description}</p>
         </div>
-      </div>
+       </div>
   }
   return (
     <div className="seven_container">
