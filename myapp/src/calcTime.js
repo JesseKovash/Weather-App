fa99dcc16a6c5fa725f97e221383ec1117281636
Jesse Kovash <jessekovash@gmail.com>
2022-05-10 12:15:47 -0600

    all data rendering on the hourly and eachhour components. Need to style both of these components. CalcTime Library is complete as well

--- myapp/src/calcTime.js
+++ myapp/src/calcTime.js
@@ -4,6 +4,13 @@ const fullDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','S
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+const hours = ['12:00 am', '1:00 am', '2:00 am', '3:00 am', '4:00 am', '5:00 am', '6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm', '8:00 pm', '9:00 pm', '10:00 pm', '11:00 pm'];
+
+const getHours = function(date) {
+  //0 index
+  return date.getHours()
+}
+
 const getDayOfWeek = function(date) {
   //0 indexed
   return date.getDay()
@@ -69,5 +76,5 @@ const isLeapYear = function(year) {
 }
 
 export {
-  days, fullDays, months, getDayOfWeek, getDayOfMonth, getMonth, getYear, getFutureWeekdays, getFutureMonthDays, numberOfDays, isLeapYear
+  days, fullDays, months, hours, getHours, getDayOfWeek, getDayOfMonth, getMonth, getYear, getFutureWeekdays, getFutureMonthDays, numberOfDays, isLeapYear
 }
--- myapp/src/components/eachHour.jsx
+++ myapp/src/components/eachHour.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { hot } from 'react-hot-loader/root';
 import { LocationContext } from '../App.js';
 import {
-  days, fullDays, months, getDayOfWeek, getDayOfMonth, getMonth, getYear, getFutureWeekdays, getFutureMonthDays, numberOfDays, isLeapYear
+  days, fullDays, months, hours, getHours, getDayOfWeek, getDayOfMonth, getMonth, getYear, getFutureWeekdays, getFutureMonthDays, numberOfDays, isLeapYear
 } from './../calcTime.js';
 
 function EachHour(props) {
@@ -11,31 +11,124 @@ function EachHour(props) {
   let dayOne = null;
   let dayTwo = null;
   let dayThree = null;
-  console.log(details)
 
-  const first = function(dayOptions) {
-    console.log(details[0])
+  function first(dayOptions) {
     let currentDay = new Date(dayOptions[0].dt * 1000);
+    let dayOfMonth = getDayOfMonth(currentDay);
+    let allHours = [];
+    for (var i=0; i<dayOptions.length; i++) {
+      let newCurrentDay = new Date(dayOptions[i].dt * 1000);
+      if (getDayOfMonth(newCurrentDay) === dayOfMonth) {
+        allHours.push(oneHrDetails(dayOptions[i]))
+      } else {
+        dayOptions = dayOptions.slice(i);
+        break;
+      }
+    }
 
     dayOne =
       <div className="hour_day">
         <span>{fullDays[getDayOfWeek(currentDay)]}, </span>
         <span>{months[getMonth(currentDay)]}</span>
         <span>{getDayOfMonth(currentDay)}</span>
+        <div className="oneHr_container">
+          {allHours}
+        </div>
       </div>
-  }(details)
 
-  const second = function(dayOptions) {
+    second(dayOptions)
+  }
+  first(details)
 
+  function second (dayOptions) {
+    let currentDay = new Date(dayOptions[0].dt * 1000);
+    let dayOfMonth = getDayOfMonth(currentDay);
+    let allHours = [];
+    for (var i=0; i<dayOptions.length; i++) {
+      let newCurrentDay = new Date(dayOptions[i].dt * 1000);
+      if (getDayOfMonth(newCurrentDay) === dayOfMonth) {
+        allHours.push(oneHrDetails(dayOptions[i]))
+      } else {
+        dayOptions = dayOptions.slice(i);
+        break;
+      }
+    }
+
+    dayTwo =
+      <div className="hour_day">
+        <span>{fullDays[getDayOfWeek(currentDay)]}, </span>
+        <span>{months[getMonth(currentDay)]}</span>
+        <span>{getDayOfMonth(currentDay)}</span>
+        <div className="oneHr_container">
+          {allHours}
+        </div>
+      </div>
+
+    third(dayOptions)
   }
 
-  const third = function(dayOptions) {
+  function third (dayOptions) {
+    let currentDay = new Date(dayOptions[0].dt * 1000);
+    let dayOfMonth = getDayOfMonth(currentDay);
+    let allHours = [];
+    for (var i=0; i<dayOptions.length; i++) {
+      let newCurrentDay = new Date(dayOptions[i].dt * 1000);
+      if (getDayOfMonth(newCurrentDay) === dayOfMonth) {
+        allHours.push(oneHrDetails(dayOptions[i]))
+      }
+    }
 
+    dayThree =
+      <div className="hour_day">
+        <span>{fullDays[getDayOfWeek(currentDay)]}, </span>
+        <span>{months[getMonth(currentDay)]}</span>
+        <span>{getDayOfMonth(currentDay)}</span>
+        <div className="oneHr_container">
+          {allHours}
+        </div>
+      </div>
+  }
+
+  function oneHrDetails(hour) {
+    let newTime = new Date(hour.dt * 1000);
+    return (
+      <div className="future_hr">
+        <div className="hour_one">
+          <p>{hours[getHours(newTime)]}</p>
+        </div>
+        <div className="hour_two">
+          <p className="hour_temp">{
+            Values.tempScale === 'F' ?
+              (Values.toFahrenheit(hour.temp)) :
+              (Values.toCelsius(hour.temp))}
+          </p>
+        </div>
+        <div className="hour_three">
+          <img className="hour_icon" src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`}></img>
+        </div>
+        <div className="hour_four">
+          <p className="hour_desc">{hour.weather[0].description}</p>
+        </div>
+
+        <p className="hour_wind">
+          <span className="hour_dir">
+            {`${Values.windDirection(hour.wind_deg)}  `}
+          </span>
+          <span className="hour_speed">{
+            Values.tempScale === 'F' ?
+              (Values.windMPH(hour.wind_speed) + ' mph') :
+              (Values.windKMH(hour.wind_speed) + ' kmh')}
+          </span>
+        </p>
+      </div>
+    )
   }
 
   return (
     <>
     {dayOne}
+    {dayTwo}
+    {dayThree}
     </>
   )
 }
