c3583921060e793d713d045bb9c06d67b69f8341
Jesse Kovash <jessekovash@gmail.com>
2022-07-22 12:41:34 -0600

    changed components/index.js to use root.render rather than reactdom.render. I believe this may have been causing it to revert to version 17

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 import "./styles.css";
 
+const mountNode = document.getElementById("app");
+// const root = ReactDOM.createRoot(mountNode);
+const root = createRoot(mountNode);
+root.render(<App name="jesses-weather" />);
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+// ReactDOM.render(<App name="Jane" />, mountNode);
