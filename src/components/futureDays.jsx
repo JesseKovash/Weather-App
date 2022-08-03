de9f6e12c859f32d4a5f00107498c3fd78a9ce86
Jesse Kovash <jessekovash@gmail.com>
2022-08-03 13:48:52 -0500

    added class and changed some CSS for bug caused by triple digit temp

--- src/components/futureDays.jsx
+++ src/components/futureDays.jsx
@@ -8,10 +8,10 @@ function FutureDays() {
   if (nextSeven) {
     dayDetails = nextSeven.map((oneDay, index) => (
       <div className="future_day" key={oneDay.moonrise}>
-        <div className="future_one">
+        <div className="future_one table_cell">
           <p>{Values?.dayOptions?.weekDays[index + 1]} {Values?.dayOptions?.monthDays[index + 1]}</p>
         </div>
-        <div className="future_two">
+        <div className="future_two table_cell">
           <p className="hi_temp">{
             Values.tempScale === 'F' ?
               (Values.toFahrenheit(oneDay.temp.max)) :
@@ -23,14 +23,14 @@ function FutureDays() {
               (Values.toCelsius(oneDay.temp.min))}&deg;
           </p>
         </div>
-        <div className="future_three">
+        <div className="future_three table_cell">
           <img className="future_icon" src={`http://openweathermap.org/img/wn/${oneDay.weather[0].icon}@2x.png`}></img>
         </div>
-        <div className="future_four">
+        <div className="future_four table_cell">
           <p className="future_desc">{oneDay.weather[0].description}</p>
         </div>
 
-        <div className="future_wind">
+        <div className="future_wind table_cell">
           <p className="future_dir">
             {`${Values.windDirection(oneDay.wind_deg)}  `}
           </p>
